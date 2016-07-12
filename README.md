f80e11f3d298335b7a75a0f12b4e992fab2374be
antonm <antonm@ca.ibm.com>
2016-07-11 21:04:49 -0400

    First Commit - Proof Of Concept

---
+++ README.md
@@ -0,0 +1,16 @@
+"# solid-winner" 
+
+
+
+Run the app locally
+
+- [Install Node.js if not done already]: https://nodejs.org/en/download/
+
+- cd into the app directory
+- Run `npm install` to install the app's dependencies
+- Update the app with your own tone analyzer service credentials
+- For writing to the Google Spreadsheet, you'll need your own API key
+- Details and examples are here: https://www.npmjs.com/package/google-spreadsheet
+- Input is a spreadsheet with columns for questions + answers and one for each emotional tone ...
+- My example spreadsheet ( John Lennon interview ) https://docs.google.com/spreadsheets/d/1UVbnv8KJ5ycYxdl_1LIr7XzFaPGhRqvf5SQRCrBqwpg/pubhtml
+- Type `node app` to run the app
---
+++ app.js
@@ -0,0 +1,96 @@
+var GoogleSpreadsheet = require('google-spreadsheet');
+var watson = require('watson-developer-cloud');
+var async = require('async');
+
+
+var doc = new GoogleSpreadsheet('1UVbnv8KJ5ycYxdl_1LIr7XzFaPGhRqvf5SQRCrBqwpg');
+
+var sheet;
+
+var answers = [];
+
+var tones = ['anger', 'disgust', 'fear', 'joy', 'sadness'];
+
+var EMOTION = 0;
+var LANGUAGE = 1;
+var SOCIAL = 2;
+
+async.series([
+  function setAuth(step) {
+        // see notes below for authentication instructions!
+        var creds = require('./google-credentials.json');
+
+        doc.useServiceAccountAuth(creds, step);
+  },
+  function getInfoAndWorksheets(step) {
+        doc.getInfo(function (err, info) {
+            console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+            sheet = info.worksheets[0];
+            console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+            step();
+        });
+  },
+  function workingWithRows(step) {
+
+        var tonecreds = require('./tone-credentials.json');
+
+        var toneAnalyzer = watson.tone_analyzer(tonecreds);
+
+        sheet.getRows({
+            offset: 1,
+            limit: 23
+        }, function (err, rows) {
+            rows.forEach(function (row) {
+
+                toneAnalyzer.tone({
+                    text: row.answer,
+                    sentences: false
+                }, function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    }
+
+
+                    var tones = data.document_tone.tone_categories[EMOTION].tones;
+
+                    tones.forEach(function (tone) {
+
+                        var score = Math.round(tone.score * 100);
+
+                        switch (tone.tone_name) {
+
+                        case 'Anger':
+                            row.anger = score;
+                            break;
+
+                        case 'Disgust':
+                            row.disgust = score;
+                            break;
+
+                        case 'Joy':
+                            row.joy = score;
+                            break;
+
+                        case 'Sadness':
+                            row.sadness = score;
+                            break;
+
+                        case 'Fear':
+                            row.fear = score;
+                            break;
+                        }
+
+                        console.log(tone.tone_name + ': ' + Math.round(tone.score * 100) + '%');
+                    });
+
+                    row.save(function (err) {
+
+                        if (err != null) {
+                            console.log(err);
+                        }
+                    });
+
+                });
+            })
+        });
+}])
